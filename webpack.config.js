6d8dd6a905fb808751718c92d4ef7e32f45c0127
Vasanth Kesavan <vk432@njit.edu>
2017-10-24 22:13:25 -0400

    hello world component with complete configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: './src/index.html',
-//   filename: 'index.html',
-//   inject: 'body'
-// });
 
 const config = {
   entry: './index.js',
